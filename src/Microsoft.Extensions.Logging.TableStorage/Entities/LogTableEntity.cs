735bb3942b21d9e1693da194faadddc150eb493c
Jorma Syrjä <jomppe_s@hotmail.com>
2020-02-10 22:28:32 +0200

    Tweaks + csproj file update for NuGet

--- src/Microsoft.Extensions.Logging.TableStorage/Entities/LogTableEntity.cs
+++ src/Microsoft.Extensions.Logging.TableStorage/Entities/LogTableEntity.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using Microsoft.Azure.Cosmos.Table;
 
 namespace Microsoft.Extensions.Logging.TableStorage.Entities
 {
     internal class LogTableEntity : TableEntity
     {
-        public string Message { get; }
-        public string Exception { get; }
-        public string StackTrace { get; }
-        public string CategoryName { get; }
-        public string LogLevel { get; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+        public string StackTrace { get; set; }
+        public string CategoryName { get; set; }
+        public string LogLevel { get; set; }
 
         public LogTableEntity(string partitionKey, string rowKey, string message, Exception exception, string categoryName, string logLevel) : base(partitionKey, rowKey)
         {
