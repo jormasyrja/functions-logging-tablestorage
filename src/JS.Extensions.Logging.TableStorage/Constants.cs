6af760185ca05748aba587154de6af73dd4eb267
Jorma Syrjä <jsyrja@wapice.com>
2020-02-12 10:15:30 +0200

    Reduced max buffer size to 100 (TableBatchOperation max)

--- src/JS.Extensions.Logging.TableStorage/Constants.cs
+++ src/JS.Extensions.Logging.TableStorage/Constants.cs
@@ -4,7 +4,7 @@
     {
         internal const string DefaultPartitionKeyDateTimeFormat = "yyyyMMdd";
         internal const int DefaultBufferSize = 10;
-        internal const int MaximumBufferSize = 1000;
+        internal const int MaximumBufferSize = 100;
         internal const int DefaultBufferTimeoutInSeconds = 10;
         internal const int MaxBufferTimeoutInSeconds = 600;
     }
